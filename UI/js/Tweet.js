28d87bee676e98f6b9d6119008df323eb545ef57
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-03-23 23:43:13 +0300

    Main task is done

---
+++ UI/js/Tweet.js
@@ -0,0 +1,216 @@
+/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-undef */
+/* eslint-disable eqeqeq */
+/* eslint-disable linebreak-style */
+class Tweet {
+  constructor(id, text, createAt, author) {
+    this._id = id;
+    this.text = text;
+    this._author = author;
+    this.createdAt = new Date(createAt);
+    this.comments = [];
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    if (typeof id === "string") {
+      this._id = id;
+    } else {
+      console.log("Cannot set id", id);
+    }
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  set createdAt(date) {
+    if (date.getTime()) {
+      this._createdAt = date;
+    } else {
+      console.log("Cannot set date", id);
+    }
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  set author(author) {
+    if (typeof author !== "string" || author.length === 0) {
+      this._author = author;
+    } else {
+      console.log("Cannot set author", author);
+    }
+  }
+
+  static validate(tw) {
+    if (Object.keys(tw).length !== 5) return false;
+
+    for (const key in tw) {
+      if (Object.hasOwnProperty.call(tw, key)) {
+        const element = tw[key];
+        switch (key) {
+          case "id":
+            if (+element <= 0) {
+              return false;
+            }
+            break;
+
+          case "text":
+          case "author":
+            if (typeof element !== "string" || element.length == 0) {
+              return false;
+            }
+            if (element.length > 280) return false;
+            break;
+
+          case "createdAt":
+            if (!element.getFullYear()) {
+              return false;
+            }
+            break;
+
+          case "comments":
+            for (let i = 0; i < element.length; i++) {
+              if (!Comment.validate(element[i])) return false;
+            }
+            break;
+          default:
+            return false;
+        }
+      } else return false;
+    }
+    return true;
+  }
+}
+
+const tweets = [
+  new Tweet("1", "Привет! #js #datamola", "2022-03-09T23:00:00", "Иванов Иван"),
+  new Tweet("2", "Как дела?", "2022-03-09T23:00:01", "Петров Петр"),
+  new Tweet(
+    "3",
+    "Подскажите торговые центры в Мюнхене",
+    "2022-03-10T13:00:00",
+    "Сорокина Лена"
+  ),
+  new Tweet(
+    "4",
+    "Где можно снять жилье недорого?",
+    "2022-03-10T14:15:40",
+    "Воробьева Агата"
+  ),
+  new Tweet(
+    "5",
+    "Если бы тут были картинки, я бы выложил свою собаку #люблюсобак",
+    "2022-03-10T15:01:04",
+    "Кураев Алексей"
+  ),
+  new Tweet(
+    "6",
+    "Переходите все на мой тг канал)))#покаInstagram",
+    "2022-03-10T15:44:04",
+    "Иванова Анастасия"
+  ),
+  new Tweet(
+    "7",
+    "Что общего у ворона и письменного стола? #help",
+    "2022-03-11T10:10:10",
+    "Кудрявец Таня"
+  ),
+  new Tweet(
+    "11",
+    "EPAM закрыл набор белоруссов... А я успел!#success",
+    "2022-03-11T16:00:04",
+    "Пупкин Макар"
+  ),
+  new Tweet(
+    "12",
+    "Читаю книжку про искусство и пью вино #жизньхороша",
+    "2022-03-11T16:01:04",
+    "Кураев Алексей"
+  ),
+  new Tweet(
+    "8",
+    "Я хотела уехать в Грузию, а сестра раскрасила мой паспорт #печаль#потрачено",
+    "2022-03-11T15:44:04",
+    "Шакун Маша"
+  ),
+  new Tweet(
+    "9",
+    "На каком языке ты пишешь?",
+    "2022-03-11T15:44:05",
+    "Славный Слава"
+  ),
+  new Tweet(
+    "10",
+    "Срочная замена паспорта стоит 100р... Грузия, жди меня! #timeforhachapury",
+    "2022-03-11T15:55:04",
+    "Шакун Маша"
+  ),
+
+  new Tweet(
+    "13",
+    "#StopWar#prayforukraine",
+    "2022-03-11T16:05:04",
+    "Булгакова Мира"
+  ),
+  new Tweet(
+    "14",
+    "Препод по философии душнила",
+    "2022-03-11T16:11:04",
+    "Петров Петр"
+  ),
+  new Tweet("15", "Знаешь ли ты?", "2022-03-11T16:44:04", "Славный Слава"),
+  new Tweet(
+    "16",
+    "Посоветуйте что почитать",
+    "2022-03-11T16:50:04",
+    "Шакун Маша"
+  ),
+  new Tweet(
+    "17",
+    "Как провести оплату в интернет-сервисе, если Mastercard и Visa не работуют? #санкции#вопрос",
+    "2022-03-11T16:52:54",
+    "Иванов Иван"
+  ),
+  new Tweet(
+    "18",
+    "Поменяла доллары, а курс упал #fail",
+    "2022-03-11T17:00:11",
+    "Воробьева Агата"
+  ),
+  new Tweet(
+    "19",
+    "Посоветуйте ресурсы для изучения js",
+    "2022-03-11T17:30:11",
+    "Иванов Иван"
+  ),
+  new Tweet(
+    "20",
+    "Наконец весна и стало теплее! Утренние пробежки, ждите меня #спорт",
+    "2022-03-11T17:55:04",
+    "Сорокина Лена"
+  ),
+  new Tweet(
+    "21",
+    "Сходила на Бетмена.Как же женщина-кошка хороша!",
+    "2022-03-11T18:00:04",
+    "Иванова Анастасия"
+  ),
+  new Tweet(
+    "22",
+    "Кто хочет вечером встретиться на чашечку чая - пишите в комментарии!",
+    "2022-03-11T18:18:18",
+    "Кураев Алексей"
+  ),
+];
+
+tweets.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
---
+++ UI/js/TweetCollection.js
@@ -0,0 +1,188 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable quotes */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable default-case */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-restricted-syntax */
+class TweetCollection {
+  constructor(twsArray = []) {
+    this._tweetsArray = twsArray;
+    this._tweetsArray.sort(
+      (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+    );
+    this._user = "";
+  }
+
+  get user() {
+    return _user;
+  }
+
+  set user(user) {
+    this._user = user;
+  }
+
+  addAll(twsArray) {
+    const wrongTweets = [];
+    if (twsArray.length > 0) {
+      twsArray.forEach((tweet) => {
+        if (Tweet.validate(tweet)) {
+          this._tweetsArray.push(tweet);
+        } else {
+          wrongTweets.push(tweet);
+        }
+      });
+    }
+    this._tweetsArray.sort(
+      (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+    );
+    return wrongTweets;
+  }
+
+  clear() {
+    this._tweetsArray.length = 0;
+  }
+
+  getPage(skip = 0, top = 10, filterConfig = {}) {
+    if (typeof skip !== "number" || typeof top !== "number") {
+      return console.log("Ошибка ввода данных в фильтр");
+    }
+    let paginationTweets = this._tweetsArray;
+    const keys = Object.keys(filterConfig);
+
+    for (const filter of keys) {
+      switch (filter) {
+        case "author":
+          paginationTweets = paginationTweets.filter(
+            (tweet) => tweet.author === filterConfig.author
+          );
+          break;
+
+        case "dateFrom":
+          paginationTweets = paginationTweets.filter(
+            (tweet) =>
+              tweet.createdAt.getTime() >= filterConfig.dateFrom.getTime()
+          );
+          break;
+
+        case "dateTo":
+          paginationTweets = paginationTweets.filter(
+            (tweet) =>
+              tweet.createdAt.getTime() <= filterConfig.dateTo.getTime()
+          );
+          break;
+
+        case "hashtags":
+          paginationTweets = paginationTweets.filter((tweet) => {
+            let result = true;
+            for (let i = 0; i < filterConfig[filter].length; i++) {
+              if (tweet.text.indexOf(`#${filterConfig[filter][i]}`) === -1) {
+                result = false;
+              }
+            }
+            return result;
+          });
+          break;
+
+        case "text":
+          paginationTweets = paginationTweets.filter(
+            (tweet) => tweet.text.indexOf(filterConfig[filter]) !== -1
+          );
+          break;
+      }
+    }
+
+    if (paginationTweets.length < skip + top) {
+      paginationTweets = paginationTweets.slice(skip, tweets.length);
+    } else {
+      paginationTweets = paginationTweets.slice(skip, top + skip);
+    }
+
+    return paginationTweets;
+  }
+
+  get(id) {
+    return (
+      this._tweetsArray.find((item) => item.id === id) ||
+      console.log(`Твит с ID=${id} не найден`)
+    );
+  }
+
+  add(text) {
+    if (this._user === "") {
+      return false;
+    }
+
+    const ID = Date.now() * 1000 + Math.floor(Math.random() * 1000);
+    if (text.length <= 280) {
+      const tw = {
+        id: ID.toString(),
+        text,
+        createdAt: new Date(),
+        author: this._user,
+        comments: [],
+      };
+      this._tweetsArray.unshift(tw);
+      return true;
+    }
+    return false;
+  }
+
+  edit(id, text) {
+    if (this._user === "") {
+      return false;
+    }
+    if (
+      text.length <= 280 &&
+      text.length > 0 &&
+      this._tweetsArray.find((item) => item.id === id) &&
+      this._tweetsArray.find((item) => item.id === id).author === this._user
+    ) {
+      this._tweetsArray.find((item) => item.id === id).text = text;
+      return true;
+    }
+    return false;
+  }
+
+  remove(id) {
+    if (this._user === "") {
+      return false;
+    }
+    for (let i = 0; i < this._tweetsArray.length; i++) {
+      if (
+        this._tweetsArray[i].id === id &&
+        this._tweetsArray[i].author === this._user
+      ) {
+        this._tweetsArray.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addComment(id, text) {
+    if (this._user === "") {
+      return false;
+    }
+    const idCom = Date.now() * 1000 + Math.floor(Math.random() * 1000);
+    const comment = new Comment(idCom.toString(), text, new Date(), this._user);
+    if (Comment.validate(comment) && this.get(id)) {
+      // this._tweetsArray.find((item) => item.id == id)?.comments.push(comment);
+      this.get(id).comments.push(comment);
+      return true;
+    }
+    return false;
+  }
+}
+
+const tweetsCollection = new TweetCollection(tweets);
+console.log(tweetsCollection.get("11"));
+tweetsCollection.user = "Кудрявец Таня";
+console.log(tweetsCollection.addComment("15", "hello"));
+console.log(tweetsCollection.add("new tweet is here!"));
+console.log(tweetsCollection.remove("44"));
+console.log(tweetsCollection.remove("12"));
+console.log(tweetsCollection.edit("7", "tweet is edited"));
+console.log(tweetsCollection.remove("7"));
+console.log(tweetsCollection.getPage(5, 15));
